46b9f6587fbe3d292fb5b757a99914e06f21c11f
asciisummer <romelsoviguidi@gmail.com>
2025-05-02 20:24:33 +0100

    feat: add function to create left node

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - insert node child left note
+ * @parent: node parent
+ * @value: value of node
+ **/
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = NULL;
+
+	if (parent == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return NULL;
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	new_node->parent = parent;
+	//
+	if (parent->left != NULL)
+	{
+		new_node->left = parent->left;
+		new_node->left->parent = new_node;
+	}
+	parent->left = new_node;
+	return new_node;
+}
--- binary_trees.h
+++ binary_trees.h
@@ -24,8 +24,10 @@ typedef struct binary_tree_s heap_t;
 
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *tree);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 #endif
