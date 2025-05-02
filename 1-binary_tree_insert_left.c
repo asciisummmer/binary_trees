21f49dc23484fb3bfc127961055c30a5b7a07559
asciisummer <romelsoviguidi@gmail.com>
2025-05-02 20:26:33 +0100

    fix: coding style

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -4,6 +4,7 @@
  * binary_tree_insert_left - insert node child left note
  * @parent: node parent
  * @value: value of node
+ * Return: new node or null if error
  **/
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
@@ -14,17 +15,16 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
-		return NULL;
+		return (NULL);
 	new_node->n = value;
 	new_node->left = NULL;
 	new_node->right = NULL;
 	new_node->parent = parent;
-	//
 	if (parent->left != NULL)
 	{
 		new_node->left = parent->left;
 		new_node->left->parent = new_node;
 	}
 	parent->left = new_node;
-	return new_node;
+	return (new_node);
 }
