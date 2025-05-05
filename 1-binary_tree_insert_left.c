176be0eb8159c7861139b4b06938bfcf04125610
asciisummer <romelsoviguidi@gmail.com>
2025-05-05 23:35:13 +0100

    feat: create node at right child of parent

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -5,7 +5,7 @@
  * @parent: node parent
  * @value: value of node
  * Return: new node or null if error
- **/
+**/
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - insert node to right parent
+ * @parent: parent of child node to insert
+ * @value: value of child
+ * Return: new node or null if error
+**/
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = NULL;
+
+	if (parent == NULL)
+		return (NULL);
+
+	node = malloc(sizeof(binary_tree_t));
+	node->n = value;
+	node->left = NULL;
+	node->right = NULL;
+	node->parent = parent;
+
+	if (parent->right != NULL)
+	{
+		node->right = parent->right;
+		parent->right->parent = node;
+	}
+	parent->right = node;
+	return (node);
+}
