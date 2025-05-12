3fd422a9603fa77ee0f36aadd1802dbf03251e2d
asciisummer <romelsoviguidi@gmail.com>
2025-05-12 22:10:29 +0100

    feat: count height of binary tree

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - determine heigth of binary tree
+ * @tree: binary tree
+ * Return: size of binary tree
+**/
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (-1);
+
+	int val_l =  binary_tree_height(tree->left);
+	int val_r =  binary_tree_height(tree->right);
+
+	if (val_l > val_r)
+		return (val_l + 1);
+	else
+		return (val_r + 1);
+}
