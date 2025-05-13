6a29b79e6b6151ef3b15d9e4b965cd852ddced3d
asciisummer <romelsoviguidi@gmail.com>
2025-05-13 22:33:31 +0100

    fix: negative value return

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,7 +9,9 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return (-1);
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
 
 	int val_l =  binary_tree_height(tree->left);
 	int val_r =  binary_tree_height(tree->right);
