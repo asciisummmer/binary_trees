c67f6bb6cb89fca8ca8607a8b319949837fac295
asciisummer <romelsoviguidi@gmail.com>
2025-05-14 14:35:49 +0100

    feat: function to get balance factor

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,37 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - determine heigth of binary tree
+ * @tree: binary tree
+ * Return: size of binary tree
+**/
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int val_l =  0;
+	int val_r =  0;
+
+	if (tree == NULL)
+		return (0);
+	val_l = binary_tree_height(tree->left);
+	val_r = binary_tree_height(tree->right);
+	if (val_l > val_r)
+		return (val_l + 1);
+	else
+		return (val_r + 1);
+}
+
+
+/**
+ * binary_tree_balance - compute balance factor or binary tree
+ * @tree: root of binary tree
+ * Return: balance factor
+**/
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+}
