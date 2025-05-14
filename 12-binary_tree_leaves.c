275819c114bad116116b07c0fe8beb42d8cd7b5e
asciisummer <romelsoviguidi@gmail.com>
2025-05-14 11:29:27 +0100

    feat: function to get number of leaves in binary tree

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - count leave of binary tree
+ * @tree: binary tree root
+ * Return: number of leaves
+**/
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	int count_leaves = 0;
+
+	if (tree == 0)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+	else
+	{
+		count_leaves += binary_tree_leaves(tree->left);
+		count_leaves += binary_tree_leaves(tree->right);
+		return (count_leaves);
+	}
+}
